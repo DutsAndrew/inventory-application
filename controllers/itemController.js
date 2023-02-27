54c3eec1e6027e684af208880712a5e81328b1f8
Andrew Dutson <Dutsandrew@gmail.com>
2023-02-26 18:15:17 -0700

    can view all items from db

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,11 +1,18 @@
-const item = require("../models/item");
+const Category = require("../models/category");
+const Item = require("../models/item");
+const async = require("async");
+const { body, validationResult } = require('express-validator');
 
-exports.index = (req, res) => {
-  res.send('Not Implemented');
-};
-
-exports.item_list = (req, res) => {
-  res.send('Not Implemented');
+exports.item_list = (req, res, next) => {
+  Item.find()
+    .sort({ name: 1 })
+    .exec(function (err, item_list) {
+      if (err) return next(err);
+      res.render("all_items", {
+        title: "All Items in Inventory",
+        items: item_list,
+      });
+    });
 };
 
 exports.item_detail = (req, res) => {
