4d96080d94a766576217051f0a400006fc3ffdd7
Andrew Dutson <Dutsandrew@gmail.com>
2023-02-26 19:13:30 -0700

    category is fetched as well in waterfall for render

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -16,19 +16,35 @@ exports.item_list = (req, res, next) => {
 };
 
 exports.item_detail = (req, res, next) => {
-  Item.findById(req.params.id)
-    .populate("name")
-    .populate("description")
-    .populate("cost")
-    .populate("amount")
-    .populate("category")
-    .exec(function (err, item) {
-      if (err) return next(err);
-      res.render("item_detail", {
-        title: "Item View",
-        item: item,
-      });
+  async.waterfall([
+    // Fetch the item by ID and populate its fields
+    (callback) => {
+      Item.findById(req.params.id)
+        .populate("name")
+        .populate("description")
+        .populate("cost")
+        .populate("amount")
+        .exec((err, item) => {
+          if (err) return callback(err);
+          callback(null, item);
+        });
+    },
+    // Fetch the category by ID and return the item and category objects
+    (item, callback) => {
+      Category.findById(item.category)
+        .exec((err, category) => {
+          if (err) return callback(err);
+          callback(null, item, category);
+        });
+    },
+  ], (err, item, category) => {
+    if (err) return next(err);
+    res.render("item_detail", {
+      title: "Item View",
+      item: item,
+      category: category,
     });
+  });
 };
 
 exports.item_create_get = (req, res) => {
