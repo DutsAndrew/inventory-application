2bb275aa9e6d87252b9c2090c4bcaa94326711af
Andrew Dutson <Dutsandrew@gmail.com>
2023-03-06 20:34:37 -0700

    production env changes

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
 
 // db hookups w/ MongoDB and Mongoose
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
-const mongoDB = 'mongodb+srv://admin:admin@cluster0.0bccweu.mongodb.net/local_inventory?retryWrites=true&w=majority';
+const dev_db_url = 'mongodb+srv://admin:admin@cluster0.0bccweu.mongodb.net/local_inventory?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 async function main() {
   await mongoose.connect(mongoDB);
@@ -26,10 +29,12 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
